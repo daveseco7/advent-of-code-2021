50627fcb07bdbb50c9d1df9693880d67fb4db968
David Fontes <david.fontes@mindera.com>
2021-12-17 23:15:30 +0000

    - implement day 15

--- day15/queue.go
+++ day15/queue.go
@@ -1,7 +1,5 @@
 package day15
 
-import "container/heap"
-
 // An Item is something we manage in a priority queue.
 type Item struct {
 	value    coordinate // The value of the item; arbitrary.
@@ -41,11 +39,4 @@ func (pq *PriorityQueue) Pop() interface{} {
 	item.index = -1 // for safety
 	*pq = old[0 : n-1]
 	return item
-}
-
-// update modifies the priority and value of an Item in the queue.
-func (pq *PriorityQueue) update(item *Item, value coordinate, priority int) {
-	item.value = value
-	item.priority = priority
-	heap.Fix(pq, item.index)
 }
